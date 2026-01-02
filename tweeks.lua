ca564d164502f31bc780c917fe00c64220336601
noskinfryday-tech <noskinfryday@gmail.com>
2026-01-02 22:35:17 +0100

    Create tweeks.lua

---
+++ tweeks.lua
@@ -0,0 +1,18 @@
+while do true
+  wait(1)
+    
+local Players = game:GetService("Players")
+  
+
+local function highlightPlayer(player)
+  local highlight = Instance.new("Highlight")
+  highlight.Parent = player.Character
+  highlight.FillColor = Color3.fromRGB(255, 0, 0)
+  highlight.OutlineColor = Color3.fromRGB(0, 0, 255)
+end
+
+for _, player in ipairs(Players:GetPlayers()) do 
+  highlightPlayer(player)
+  print("Nice")
+end
+end
